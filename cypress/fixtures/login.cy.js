a81c0b2873a6739ca9438ce7644fd19ee665777c
siluyanovQA <siluyanov@udev.dev>
2024-09-04 22:22:27 +0400

    clean up tests

--- cypress/fixtures/login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Login Test for Kinescope', () => {
-    it('should log in successfully with valid credentials', () => {
-      // Visit the Kinescope login page
-      cy.visit('https://app.kinescope.io/');
-      cy.get('input[placeholder="Почта"]').type('sergeysiluyanov@yandex.ru');
-      cy.get('input[placeholder="Пароль"]').type('Ss09011992Af!');
-      cy.get('button[type="submit"]').click();
-    });
-  });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,8 @@
 Cypress.Commands.add('login', () => {
-    cy.visit('https://app.kinescope.io/');
-    cy.get('input[placeholder="Почта"]').type('sergeysiluyanov@yandex.ru');
-    cy.get('input[placeholder="Пароль"]').type('Ss09011992Af!');
-    cy.get('button[type="submit"]').click();
+    cy.fixture('login').then((login) => {
+        cy.visit('https://app.kinescope.io/');
+        cy.get('input[placeholder="Почта"]').type(login.email);
+        cy.get('input[placeholder="Пароль"]').type(login.password);
+        cy.get('button[type="submit"]').click();
+    });
 });
