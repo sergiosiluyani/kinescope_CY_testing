ecd73d70cb76c28f39b04b7adb34fe99dd6f4ea3
sergiosiluyani <120129716+sergiosiluyani@users.noreply.github.com>
2024-09-09 10:49:01 +0400

    Update 4_verifyVideoUpdate.cy.js
    update

--- cypress/e2e/4_verifyVideoUpdate.cy.js
+++ cypress/e2e/4_verifyVideoUpdate.cy.js
@@ -11,14 +11,10 @@ describe('Verify the video is uploaded', () => {
         cy.fixture('login').then((credentials) => {
           cy.login(credentials);  // Assuming cy.login() is a custom command
         });
-        
-      
-        // Step 2: user visits the page where the video is uploaded
-        cy.visit('https://app.kinescope.io/video');
 
         cy.getFolder();
         
-        // Step 3: user verifies the video with the correct title is present
+        // Step 2: user verifies the video with the correct title is present
         cy.fixture('updatedVideoInfo').then((data) => {
 
           expect(data).to.have.property('updatedTitle');
